32c5a6da01b8ec4a86fd4835087ff64090971b81
Juan Batiz-Benet <juan@benet.ai>
2015-08-13 04:20:06 +0200

    v1

--- cli.js
+++ cli.js
@@ -1,9 +1,53 @@
 #!/usr/bin/env node
 
+var usage = 'Usage: asciinema-selfhost --clone <asciinema-id> [<path>]\n'
+  + '       asciinema-selfhost --rehost <asciinema-id>\n\n'
+  + 'Clone asciinemas as small static websites, and then self host them\n'
+  + 'on any webserver. Or use --rehost to publish them straight to ipfs!'
+
 var as = require('./')
+var opt = require('optimist')
+  .usage(usage)
+  .boolean('h').alias('h', 'help').describe('h', 'show this help output')
+  .boolean('c').alias('c', 'clone').describe('c', 'clone asciinema')
+  .boolean('j').alias('j', 'json').describe('j', 'fetch asciinema.json')
+  .boolean('s').alias('s', 'size').describe('s', 'fetch asciinema size')
+  .boolean('r').alias('r', 'rehost').describe('r', 'rehost asciinema to ipfs')
 
-as.getAsciinemaJSON(process.argv[2], function(err, data) {
-  if (err) throw err
+if (opt.argv.h || opt.argv._.length < 1) {
+  process.stdout.write(opt.help())
+  process.exit(0)
+}
 
-  console.log(JSON.stringify(data))
-})
+if (opt.argv.j) {
+
+  as.getAsciinemaJSON(opt.argv._[0], function(err, data) {
+    if (err) throw err
+    process.stdout.write(JSON.stringify(data))
+    process.stdout.write('\n')
+  })
+
+} else if (opt.argv.s) {
+
+  as.getAsciinemaSize(opt.argv._[0], function(err, data) {
+    if (err) throw err
+    process.stdout.write(JSON.stringify(data))
+    process.stdout.write('\n')
+  })
+
+} else if (opt.argv.c) {
+
+  var path = (opt.argv._.length > 1) ? opt.argv._[1] : null
+  as.cloneAsciinema(opt.argv._[0], path, function(err, data) {
+    if (err) throw err
+  })
+
+} else if (opt.argv.r) {
+
+  as.rehostAsciinema(opt.argv._[0], function(err, hash) {
+    if (err) throw err
+
+    console.log('view locally at http://localhost:8080/ipfs/' + hash)
+    console.log('view globally at http://gateway.ipfs.io/ipfs/' + hash)
+  })
+}
--- index.js
+++ index.js
@@ -1,11 +1,31 @@
+var fs = require('fs')
+var fse = require('fs-extra')
+var path = require('path')
+var vinyl = require('vinyl')
+var vinylfs = require('vinyl-fs')
+var ipfsApi = require('ipfs-api')
 var request = require('request')
+var cheerio = require('cheerio')
 
 var x = module.exports = {}
 x.getAsciinemaJSON = getAsciinemaJSON
+x.getAsciinemaFrame = getAsciinemaFrame
+x.getAsciinemaSize = getAsciinemaSize
+x.cloneAsciinema = cloneAsciinema
+x.rehostAsciinema = rehostAsciinema
 
-function getAsciinemaJSON(url, cb) {
+var embedUrl = "https://asciinema.org/api/asciicasts/"
+var pageUrl = "https://asciinema.org/a/"
+
+function getAsciinemaJSON(id, cb) {
+  var url = id
+  if (url.match(/^[a-zA-Z0-9]+$/))
+    url = pageUrl + url
+
   request(url, function(err, res) {
     if (err) return cb(err)
+    if (res.statusCode != 200)
+      return cb(new Error("status code not 200"))
 
     var m = res.body.match(/https:\/\/asciinema-bb-eu[^"]+/gi)
     if (!m || !m[0]) return cb(new Error('no valid player json url in ' + url))
@@ -14,6 +34,116 @@ function getAsciinemaJSON(url, cb) {
       if (err) return cb(err)
 
       cb(null, JSON.parse(res.body))
+    })
+  })
+}
+
+function getAsciinemaFrame(size) {
+  var frame = fs.readFileSync(__dirname + "/tmpl/iframe.html").toString()
+  if (size.height && size.width) {
+    var str = 'height:'+ size.height +'px; width:'+ size.width +'px'
+    frame = frame.replace('style="', 'style="' + str)
+  }
+  return frame
+}
+
+function getAsciinemaSize(id, cb) {
+  var url = id
+  if (url.match(/^[a-zA-Z0-9]+$/))
+    url = embedUrl + url
+
+  request(url, function(err, res) {
+    if (err) return cb(err)
+    if (res.statusCode != 200)
+      return cb(new Error("status code not 200"))
+
+    var match = res.body.match(/"width":[0-9]+,"height":[0-9]+}/)
+    if (!match) return cb(new Error("cannot parse width/height from "+url))
+
+    var json = JSON.parse('{'+match[0])
+    if (!json.width || !json.height)
+      return cb(new Error("parse error: missing width or height"), json)
+
+    cb(null, json)
+  })
+}
+
+function cloneAsciinema(id, fpath, cb) {
+  id = id.toString().split('/')
+  id = id[id.length - 1]
+  if (!fpath) fpath = id
+
+  console.log('cloning', id, 'to', fpath)
+
+
+  preparePath(fpath, function(err) {
+    if (err) return cb(err)
+
+    getAsciinemaJSON(id, function(err, json) {
+      if (err) return cb(err)
+
+      logWrite(fpath + '/data/asciinema.json', JSON.stringify(json), function(err) {
+        if (err) return cb(err)
+
+        getAsciinemaSize(id, function(err, size) {
+          if (err) return cb(err)
+
+          logWrite(fpath + '/data/size.json', JSON.stringify(size), function(err) {
+            if (err) return cb(err)
+
+            var frame = getAsciinemaFrame(size)
+            logWrite(fpath + '/iframe.html', frame, function(err) {
+              if (err) return cb(err)
+              cb(null, fpath)
+            })
+          })
+        })
+      })
+    })
+  })
+}
+
+function logWrite(fpath, data, cb) {
+  console.log('writing ' + fpath)
+  fse.mkdirp(path.dirname(fpath), function(err) {
+    if (err) return cb(err)
+    fs.writeFile(fpath, data, cb)
+  })
+}
+
+function preparePath(fpath, cb) {
+  // console.log('preparing', fpath)
+  fse.mkdirp(fpath, function(err) {
+    if (err) return cb(err)
+    console.log('copying', __dirname + '/build', 'to', fpath)
+    fse.copy(__dirname + '/build', fpath, cb)
+  })
+}
+
+function rehostAsciinema(id, opts, cb) {
+  if (!id) throw new Error("no asciinema id given")
+  if (typeof(opts) === 'function') {
+    cb = opts
+    opts = {}
+  }
+  opts = opts || {}
+
+  id = id.split('/')
+  id = id[id.length - 1]
+  var ipfs = ipfsApi(opts.host, opts.port)
+
+  var fpath = '/tmp/asciinema/' + id
+  cloneAsciinema(id, fpath, function(err, fpath) {
+    if (err) return cb(err)
+
+    console.log('\nadding to ipfs. make sure your ipfs daemon is running.')
+    console.log('ipfs add -r', fpath)
+    ipfs.add(fpath, {recursive: true}, function(err, res) {
+      if (err) return cb(err)
+
+      var last = res[res.length-1]
+      console.log('published to /ipfs/' + last.Hash)
+      cb(null, last.Hash)
     })
   })
 }
